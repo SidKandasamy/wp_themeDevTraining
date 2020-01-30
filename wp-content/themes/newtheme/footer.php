4ce13d7156de34cd54299058713fa78b64595147
Sid Kandasamy <sidkandasamy@outlook.com>
2020-01-30 15:14:39 +0000

    initial start up with theme development

---
+++ wp-content/themes/newtheme/footer.php
@@ -0,0 +1,4 @@
+<?php wp_footer();?> <!-- hook that plugin plug into to add js script at the bottom of doc -->
+
+</body>
+</html>
---
+++ wp-content/themes/newtheme/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+    
+        <?php wp_head();?>
+
+    </head>
+
+<body <?php body_class();?>> <!--allows wordpress to add its own body class -->
+    
