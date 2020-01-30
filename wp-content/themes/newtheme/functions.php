a26c3855204a4735cfd27bf27adf9163695642ec
Sid Kandasamy <sidkandasamy@outlook.com>
2020-01-30 15:51:02 +0000

    css working

--- wp-content/themes/newtheme/functions.php
+++ wp-content/themes/newtheme/functions.php
@@ -3,9 +3,15 @@
 function load_stylesheets()
 {
 
-    wp_register_style('stylesheet', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
-    wp_enqueue_style('stylesheet');
+  
+    wp_register_style('boostrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
+    wp_enqueue_style('bootstrap');
 
+    //implementing bootstrap. 
+
+    wp_register_style('style', get_template_directory_uri() . '/style.css', array(), false, 'all');
+    wp_enqueue_style('style');
+    //implementing our own CSS
 
 }
 add_action('wp_enqueue_scripts', 'load_stylesheets');
